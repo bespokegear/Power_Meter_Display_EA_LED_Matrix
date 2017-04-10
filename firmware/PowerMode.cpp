3052b59b24c27358dedb88464160ec169d1b8f47
Matthew Gates <matthewg42@gmail.com>
2017-04-10 16:13:06 +0100

    prepare for power graph

--- firmware/PowerMode.cpp
+++ firmware/PowerMode.cpp
@@ -9,20 +9,20 @@ void PowerMode_::start(const char* data)
 {
     DB(F("PowerMode::start "));
     DBLN(data);
-
-    // Some constants we might want to tweak
-    const uint8_t fontSize = 4;
-    const uint8_t oneWidth = 3;
-    const uint8_t otherWidth = 6;
-    const uint8_t ypos = 18;
-    const uint8_t xpos = 37; // the position of the decimal point
-    
     String s = data;
     // strip trailing '-' characters
     s.replace("-", "");
+    Matrix.clear();
+    // TODO: determine if we need to clear the graph
+    // TODO: plot graph 
+    writeValue(s.toInt());
+    Matrix.paint();
+}
 
+void PowerMode_::writeValue(uint32_t tenthsWatt)
+{
     // Work out width in pixels of whole part text
-    uint16_t wholePart = s.toInt()/10;
+    uint16_t wholePart = tenthsWatt/10;
     uint8_t wholePartWidth = wholePart == 0 ? otherWidth+1 : 0;
     for (uint16_t i=wholePart; i>0; i/=10) {
         wholePartWidth += (i%10==1) ? oneWidth : otherWidth;
@@ -30,15 +30,16 @@ void PowerMode_::start(const char* data)
     }
 
     Matrix.setFont(fontSize);
-    Matrix.clear();
     // Write the whole part
-    Matrix.text(MATRIX_ORANGE, xpos - wholePartWidth, ypos, String(s.toInt()/10));
+    Matrix.text(MATRIX_GREEN, xpos - wholePartWidth, ypos, String(wholePart));
     // Draw the decimal point
-    Matrix.rectangle(MATRIX_ORANGE, xpos, ypos, 2, 2);
+    Matrix.rectangle(MATRIX_GREEN, xpos, ypos, 2, 2);
     // Write the fractional part (1 d.p.)
-    Matrix.text(MATRIX_ORANGE, xpos+3, ypos, String(s.toInt()%10));
+    Matrix.text(MATRIX_GREEN, xpos+3, ypos, String(tenthsWatt%10));
     // Write the units symbol (Watts)
-    Matrix.text(MATRIX_ORANGE, xpos+12, ypos, "W");
-    Matrix.paint();
+    Matrix.text(MATRIX_GREEN, xpos+12, ypos, "W");
 }
 
+void PowerMode_::update()
+{
+}
--- firmware/PowerMode.h
+++ firmware/PowerMode.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+#include "Config.h"
 #include "DisplayMode.h"
 
 class PowerMode_ : public DisplayMode 
@@ -7,6 +9,25 @@ class PowerMode_ : public DisplayMode
 public:
     PowerMode_() {;}
     void start(const char* data);
+    void stop();
+    void writeValue(uint32_t tenthsWatt);
+    void update();
+    //void clearGraph();
+    //void plotGraph();
+    //void addValue;
+
+private:
+   // Some constants we might want to tweak
+    static const uint8_t fontSize = 4;
+    static const uint8_t oneWidth = 3;
+    static const uint8_t otherWidth = 6;
+    static const uint8_t ypos = 18;
+    static const uint8_t xpos = 37; // the position of the decimal point
+
+    unsigned long nextGraphUpdate; 
+    uint32_t graphData;
+    uint8_t graphPos;
+    uint32_t graphMax;
 
 };
 
