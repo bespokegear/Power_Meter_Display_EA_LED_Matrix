b271d2aaa52545523edb46ec137c425b1c7354d0
Matthew Gates <matthewg42@gmail.com>
2017-04-11 19:11:49 +0100

    move font metrics into Matrix class

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -9,7 +9,6 @@
 #include "Matrix.h"
 #include "Parser.h"
 #include "Settings.h"
-#include "FontData.h"
 
 SoftwareSerial softSerial(SW_SERIAL_RX, SW_SERIAL_TX);
 Heartbeat heartbeat(HEARTBEAT_PIN);
@@ -21,9 +20,6 @@ void setup() {
     Parser.begin();
     Serial.print(F("Max Graph Power: "));
     Serial.println(MaxPowerWatts.get());
-    DB(F("Width of \"hello\" in font 2 = "));
-    DBLN(getStringWidth(2, "hello"));
-    Matrix.text(MATRIX_RED, 0, 15, "hello");
     delay(100);
     DBLN(F("setup:E"));
 }
