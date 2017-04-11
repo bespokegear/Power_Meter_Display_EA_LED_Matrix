31023971ecc6bf97050b629af2cc709042a4fd00
Matthew Gates <matthewg42@gmail.com>
2017-04-11 11:11:52 +0100

    visual tweaks to CountdownMode

--- firmware/CountdownMode.cpp
+++ firmware/CountdownMode.cpp
@@ -13,26 +13,26 @@ void CountdownMode_::start(const char* data)
     DBLN(data);
     Matrix.clear();
     Matrix.paint();
-    Matrix.setFont(5);
+    Matrix.setFont(6);
 }
 
 void CountdownMode_::update()
 {
     unsigned long next = startMs + (i*2);
     if (millis() >= next && i < 18) {
-        uint8_t bgCol[3] = { MATRIX_GREEN, MATRIX_ORANGE, MATRIX_RED };
-        uint8_t txCol[3] = { MATRIX_RED, MATRIX_GREEN, MATRIX_ORANGE };
-        uint8_t colIdx = n % 3;
+        uint8_t bgCol[2] = { MATRIX_GREEN, MATRIX_RED };
+        uint8_t txCol[2] = { MATRIX_RED, MATRIX_GREEN };
+        uint8_t colIdx = n % 2;
         i+=1;
         Matrix.rectangle(bgCol[colIdx], 17-i, 15+i, 30+(i*2), i*2);
 
         // At the end of the sequence, draw the text
         if (i==17) {
             if (n==0) {
-                Matrix.text(txCol[colIdx], 20, 23, "Go!");
+                Matrix.text(txCol[colIdx], 18, 23, "GO!");
             } else if (n==1) {
                 // digit '1' is narrower than other digits
-                Matrix.text(txCol[colIdx], 29, 23, String(n));
+                Matrix.text(txCol[colIdx], 28, 23, String(n));
             } else {
                 Matrix.text(txCol[colIdx], 27, 23, String(n));
             }
