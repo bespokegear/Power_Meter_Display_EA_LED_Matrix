3f4e1e658539adb72fe7baafc44348aa71f40ad9
Matthew Gates <matthewg42@gmail.com>
2017-04-12 14:01:20 +0100

    character wrap is now word wrap

--- firmware/TextMode.cpp
+++ firmware/TextMode.cpp
@@ -8,7 +8,7 @@ TextMode_ TextMode;
 
 TextMode_::TextMode_() :
     _fontSize(1),
-    _justify('R'),
+    _justify('L'),
     _color(MATRIX_ORANGE)
 {
 }
@@ -27,16 +27,46 @@ void TextMode_::start(const char* data)
     // But this actually depends on the font
     uint8_t maxLines = Matrix.getCharacterHeight(MATRIX_SIZE_Y / (_fontSize+1));
     uint8_t lines = 0;
+    int8_t lastSpace = -1; // The last space character in the current line
     for (uint8_t i=0; data[i]!=0; i++) {
-        if (data[i] == '\r' || data[i] == '\n' || data[i] == '|' || Matrix.getStringWidth(_fontSize, lineText[lines]) + Matrix.getCharacterWidth(_fontSize, data[i]) >= MATRIX_SIZE_X) {
+        if (data[i] == '\r' || data[i] == '\n' || data[i] == '|') {  
+            // explicit line break
             if (lines == 4) {
+                // if we're out of lines, stop adding more letters
+                break;
+            } else {
+                // continue on next line
+                lines++;
+                lastSpace = -1;
+                continue;
+            }
+        } else if (Matrix.getStringWidth(_fontSize, lineText[lines]) + Matrix.getCharacterWidth(_fontSize, data[i]) >= MATRIX_SIZE_X) {
+            // word wrap
+            if (lines == 4) {
+                // if we're out of lines, stop adding more letters
                 break;
             }
+            if (lastSpace>=0) {
+                // 1. copy last partial word from the end of this line to the next line
+                lineText[lines+1] = lineText[lines].substring(lastSpace+1);
+                // 2. remove last partial word from end of this line
+                lineText[lines].remove(lastSpace);
+                lastSpace = -1;
+            }
+            // 3. text from here goes on next line
             lines++;
         }
         lineText[lines] += data[i];
+        if (data[i] == ' ') {
+            lastSpace = lineText[lines].length() - 1;
+            DB("Current line=\"");
+            DB(lineText[lines]);
+            DB("\" lastSpace idx=");
+            DBLN(lastSpace);
+        }
     }
     lines++;
+
     // Work out where to put it
     uint8_t ysize = lines * (1 + Matrix.getCharacterHeight(_fontSize));
     uint8_t ybottom = MATRIX_SIZE_Y - ((MATRIX_SIZE_Y - ysize)/2);
