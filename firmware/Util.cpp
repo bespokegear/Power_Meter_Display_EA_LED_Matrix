9c332699297ed9f458432a796ac660df3d3cbd9c
Matthew Gates <matthewg42@gmail.com>
2017-04-17 13:51:58 +0100

    fix handling of badly formed parameter

---
+++ firmware/Util.cpp
@@ -0,0 +1,25 @@
+#include "Util.h"
+
+#include <stdint.h>
+
+bool isInteger(const String s)
+{
+    for(uint16_t i=0; i<s.length(); i++) {
+        if (s[i] < '0' || s[i] > '9') {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isInteger(const char* s)
+{
+    for(uint16_t i=0; s[i]!=0; i++) {
+        if (s[i] < '0' || s[i] > '9') {
+            return false;
+        }
+    }
+    return true;
+}
+
+
---
+++ firmware/Util.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Arduino.h>
+
+bool isInteger(const String s);
+bool isInteger(const char* s);
+
+
